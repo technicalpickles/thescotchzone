af259bb4cd200aa7e7054156c66ad1a4b8bb4f75
Joshua Nichols <josh@technicalpickles.com>
2009-11-04 12:35:33 -0500

    Added welcome controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :scotches
-  map.root :controller => :scotches
-
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  map.root :controller => :welcome, :action => :show
 end
