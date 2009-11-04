8e3f470d14b307e621af2a7c3207eb898bfa33c3
Joshua Nichols <josh@technicalpickles.com>
2009-11-04 01:55:19 -0500

    Use default inherited_resources controller.

--- app/controllers/scotches_controller.rb
+++ app/controllers/scotches_controller.rb
@@ -1,21 +1,2 @@
 class ScotchesController < InheritedResources::Base
-  
-  def index
-    @scotches = Scotch.all
-  end
-
-  def new
-    @scotch = Scotch.new
-  end
-  
-  def create
-    @scotch = Scotch.create! params[:scotch]
-
-    redirect_to @scotch
-  end
-
-  def show
-    @scotch = Scotch.find params[:id] 
-  end
-
 end
