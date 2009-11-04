49d012507b04b8263b771303f799983399876b95
Joshua Nichols <josh@technicalpickles.com>
2009-11-04 01:18:07 -0500

    Finished basic create/show/index.

--- test/functional/scotches_controller_test.rb
+++ test/functional/scotches_controller_test.rb
@@ -25,4 +25,57 @@ class ScotchesControllerTest < ActionController::TestCase
 
   end
 
+  context "new" do
+    setup do
+      get :new
+    end
+
+    should_respond_with :success
+    should_render_template :new
+    should_assign_to :scotch, :class => Scotch
+
+    should "render form to create" do
+      url = scotches_path
+      assert_select "form[action=#{url}][method=post]" do
+        assert_select "input[type=text][name='scotch[name]']"
+        assert_select "input[type=text][name='scotch[age]']"
+        assert_select "input[type=submit]"
+      end
+
+    end
+  end
+
+  context "create" do
+    setup do
+      post :create, :scotch => { :name => 'Dalmour', :age => 12 }
+      @scotch = Scotch.find_by_name('Dalmour')
+    end
+
+    should_redirect_to('the scotch index') { scotch_url(@scotch) }
+
+    should "ensure a scotch was created" do
+      assert @scotch
+      assert_equal 'Dalmour', @scotch.name
+      assert_equal 12, @scotch.age
+    end
+  end
+
+  context "show" do
+    setup do
+      @scotch = Scotch.create! :name => 'Glennfiddich', :age => 18
+
+      get :show, :id => @scotch.id
+    end
+
+    should_respond_with :success
+    should_render_template :show
+    should_assign_to(:scotch) { @scotch }
+
+    should "display heading with name" do
+      assert_select "h1", "Glennfiddich"
+    end
+
+  end
+
+
 end
