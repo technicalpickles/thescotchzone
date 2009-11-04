8f86f02822217bba608b01bb097a6c2e109d5a2e
Joshua Nichols <josh@technicalpickles.com>
2009-11-04 18:40:19 -0500

    Mixed in some formtastic.

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.gem 'inherited_resources'
   config.gem 'clearance'
+  config.gem 'formtastic'
   config.time_zone = 'UTC'
 end
 
