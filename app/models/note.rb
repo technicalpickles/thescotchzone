c9e5595ef25f1f644d8f181ccd90e5696645e9f4
Joshua Nichols <josh@technicalpickles.com>
2009-11-07 03:29:37 -0500

    Made note require appropriate attributes.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,6 @@
 class Note < ActiveRecord::Base
   belongs_to :scotch
   belongs_to :user
+
+  validates_presence_of :body, :scotch, :user
 end
--- test/unit/note_test.rb
+++ test/unit/note_test.rb
@@ -3,4 +3,5 @@ require 'test_helper'
 class NoteTest < ActiveSupport::TestCase
   should_belong_to :scotch
   should_belong_to :user
+  should_validate_presence_of :body, :scotch, :user
 end
