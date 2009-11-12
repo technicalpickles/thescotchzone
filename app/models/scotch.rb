5af8c8bcb3f599e1448d792964bd1c3cbd18d9ca
Joshua Nichols <josh@technicalpickles.com>
2009-11-11 22:27:10 -0500

    Re-arranged scotch.

--- app/models/scotch.rb
+++ app/models/scotch.rb
@@ -1,10 +1,10 @@
 class Scotch < ActiveRecord::Base
   validates_presence_of :name
 
-  has_friendly_id :name, :use_slug => true
-
   has_many :notes
   has_many :users, :through => :notes
+
+  has_friendly_id :name, :use_slug => true
 
   def to_s
     name
