5af0b4d14aa7587914a73844dbe920780a61d4cb
Joshua Nichols <josh@technicalpickles.com>
2009-11-13 22:09:57 -0500

    Put back config.gem for now.

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,14 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem "friendly_id"
+  config.gem 'inherited_resources'
+  config.gem 'clearance'
+  config.gem 'formtastic'
+  config.gem 'title_estuary'
+  config.gem 'searchlogic'
+  config.gem 'will_paginate'
+  
   config.time_zone = 'UTC'
 end
 
