32cb3edac849c165e3146d48f205288f390789ea
Joshua Nichols <josh@technicalpickles.com>
2010-10-30 23:53:25 -0400

    Try securing cookies.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   include Clearance::Authentication
   protect_from_forgery
+
+  def sign_in(user)
+    if user
+      cookies[:remember_token] = {
+        :value => user.remember_token,
+        :expires => Clearance.configuration.cookie_expiration.call,
+        :secure => true
+      }
+      self.current_user = user
+    end
+  end
 end
