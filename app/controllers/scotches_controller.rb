84774c243dea08f5f7210e198d41c06039c1a510
Joshua Nichols <josh@technicalpickles.com>
2010-12-13 00:49:04 -0500

    Require auth for scotches.

--- app/controllers/scotches_controller.rb
+++ app/controllers/scotches_controller.rb
@@ -1,6 +1,8 @@
 class ScotchesController < InheritedResources::Base
   respond_to :html, :xml, :json, :mobile
 
+  before_filter :authenticate, :only => [:new, :create, :edit, :destroy]
+
   def create
     create! do |format|
       format.html { redirect_to @scotch }
