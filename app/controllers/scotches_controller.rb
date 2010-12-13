ec452134838a014721c8b31d51e3ee6d54e2444a
Joshua Nichols <josh@technicalpickles.com>
2010-12-13 00:48:21 -0500

    More mobile moarrr.

--- app/controllers/scotches_controller.rb
+++ app/controllers/scotches_controller.rb
@@ -1,6 +1,13 @@
 class ScotchesController < InheritedResources::Base
   respond_to :html, :xml, :json, :mobile
 
+  def create
+    create! do |format|
+      format.html { redirect_to @scotch }
+      format.mobile { redirect_to @scotch }
+    end
+  end
+
   def show
     @note = Note.new
 
