0d7d285bdefb0bfeacc6ea34423ffbb09e7cca73
Joshua Nichols <josh@technicalpickles.com>
2010-12-13 00:44:05 -0500

    Can leave notes using mobile.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,11 +1,15 @@
 class NotesController < InheritedResources::Base
   belongs_to :scotch, :user, :optional => true
+  respond_to :html, :xml, :json, :mobile
 
   before_filter :authenticate, :only => [:new, :create, :edit, :destroy]
   
 
   def create
-    create! { @note.scotch }
+    create! do |format|
+      format.html { redirect_to @note.scotch }
+      format.mobile { redirect_to @note.scotch }
+    end
   end
 
 protected
