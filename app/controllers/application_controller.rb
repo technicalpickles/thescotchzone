087bf70d709b4ba238add320dc31d452307cdde8
Joshua Nichols <josh@technicalpickles.com>
2010-12-13 00:36:26 -0500

    Fix authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,6 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery
 
-  def sign_in(user)
-    if user
-      cookies[:remember_token] = {
-        :value => user.remember_token,
-        :expires => Clearance.configuration.cookie_expiration.call,
-        :secure => true
-      }
-      self.current_user = user
-    end
-  end
-
   def set_mobile_format
     if is_mobile_device? # && !request.xhr?
       request.format = session[:mobile_view] == false ? :html : :mobile
